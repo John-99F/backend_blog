e048e51942bbc649aa446a7f1432b7bb17c3f1b3
John Muñoz <jmunoztopa@gmail.com>
2025-11-18 17:01:08 -0500

    Cambios

--- app/util/security_utils.py
+++ app/util/security_utils.py
@@ -44,7 +44,7 @@ def expires_times():
 
 
 # ----- OAuth2 (ruta correcta según tu controlador) -----
-authToken = OAuth2PasswordBearer(tokenUrl="/users/token")
+authToken = OAuth2PasswordBearer(tokenUrl="token")
 
 
 # ----- VERIFICAR TOKEN -----
