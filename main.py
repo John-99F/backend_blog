adb1e05a58e9841ae7ee70af09b8909c1b63c628
John Muñoz <jmunoztopa@gmail.com>
2025-11-17 21:23:30 -0500

    se ajusta el main

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ from fastapi import FastAPI
 from app.data.database import Base, engine
 from app.controller import user_controller, blogs_posts_controller
 
-app = FastAPI(
-    title="Backend Blog IA",
-    description="Trabajo final backend",
-    version="1.0.0"
-)
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Backend Blog IA",
+        description="Trabajo final backend",
+        version="1.0.0"
+    )
 
+    # Crear tablas si no existen
+    Base.metadata.create_all(bind=engine)
 
-#Crea la base de datos 
-Base.metadata.create_all(bind=engine)
+    # Routers
+    app.include_router(user_controller)
+    app.include_router(blogs_posts_controller)
 
+    return app
 
-app.include_router(user_controller)
-app.include_router(blogs_posts_controller)
-
-
-if __name__ == "__main__":
-    import uvicorn
-    print("Servidor corriendo en http://localhost:8080")
-    uvicorn.run("app.main:app",host="0.0.0.0", port=8080, reload=True)
+app = create_app()
