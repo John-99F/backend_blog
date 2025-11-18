3d22219bb0fdfa82dc733da961c6b9dbb6477795
John Muñoz <jmunoztopa@gmail.com>
2025-11-17 22:02:08 -0500

    Se soluciona error en blogs

--- app/controller/blogs_posts_controller.py
+++ app/controller/blogs_posts_controller.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter, HTTPException, Body, Depends
+from sqlalchemy.orm import Session
 from app.services.blogs_posts_services import get_all_post, generate_post
 from app.util.security_utils import verificar_token
+from app.data.database import get_db
 
 router = APIRouter(prefix="/blog", tags=["Blog"])
 
 
 # Obtener todos los posts
 @router.get("/posts")
-def get_posts():
+def get_posts(db: Session = Depends(get_db)):
     try:
-        return get_all_post()
+        return get_all_post(db)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -18,13 +20,14 @@ def get_posts():
 @router.post("/generate-post")
 def create_post(
     prompt_data: dict = Body(...),
-    token: str = Depends(verificar_token)
+    current_user: str = Depends(verificar_token),
+    db: Session = Depends(get_db)
 ):
     try:
         if "prompt" not in prompt_data:
             raise HTTPException(status_code=422, detail="El campo 'prompt' es obligatorio")
 
-        response = generate_post(prompt_data["prompt"])
+        response = generate_post(prompt_data["prompt"], current_user, db)
 
         return {
             "status": "success",
--- app/services/blogs_posts_services.py
+++ app/services/blogs_posts_services.py
@@ -1,40 +1,35 @@
-from app.data.database import Base, engine, get_db
 from sqlalchemy.orm import Session
-from fastapi import HTTPException, Depends
+from fastapi import HTTPException
 from app.services.google_services import generar_blog
 from app.data.models.articles import Article
 import json
 
 
-def get_all_post(db: Session = Depends(get_db)): 
+def get_all_post(db: Session):
     articles = db.query(Article).all()
     return [a.to_dict() for a in articles]
 
 
-def generate_post(
-    prompt: str,
-    current_user: str,     # ID o email del usuario extraído del JWT
-    db: Session = Depends(get_db)
-):
+def generate_post(prompt: str, current_user: str, db: Session):
 
-    # 1. Llamar API de IA
+    # 1. Generar contenido con IA
     result = generar_blog(prompt)
 
-    # 2. Validar JSON que devuelve Gemini
+    # 2. Validar JSON
     try:
         json_data = json.loads(result)
     except Exception:
         raise HTTPException(
-            status_code=500, 
+            status_code=500,
             detail="La IA no devolvió un JSON válido"
         )
 
-    # 3. Guardar artículo en BD
+    # 3. Crear artículo
     post = Article(
         title=json_data.get("title"),
         description=json_data.get("description"),
         imageurl=json_data.get("imageurl"),
-        author_id=current_user  # viene del token JWT
+        author_id=current_user
     )
 
     db.add(post)
